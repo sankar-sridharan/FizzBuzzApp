d6fb48aa0ad196ebbedaf0fadf94f8f74c89709f
sankar-sridharan <38471169+sankar-sridharan@users.noreply.github.com>
2024-04-28 09:41:05 +0100

    updates

--- Program.cs
+++ Program.cs
@@ -6,11 +6,8 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-
         // Add services to the container.
-
         builder.Services.AddSingleton<FizzBuzzService>();
-       // builder.Services.AddSingleton<FizzBuzzServiceFactory>();
         builder.Services.AddControllersWithViews();
         builder.Services.AddRazorPages(options =>
         {
@@ -18,7 +15,6 @@ public class Program
         });
 
         var app = builder.Build();
-
         // Configure the HTTP request pipeline.
         if (!app.Environment.IsDevelopment())
         {
@@ -26,19 +22,13 @@ public class Program
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
         }
-
         app.UseHttpsRedirection();
         app.UseStaticFiles();
-
         app.UseRouting();
-
         app.UseAuthorization();
 
-        //app.MapControllerRoute(
-        //    name: "default",
-        //    pattern: "{controller=Home}/{action=Index}/{id?}");
         app.MapRazorPages();
-        
+       
 
         app.Run();
     }
